fa7b5238a54c4d5a7c2dc4c316ff11dfe91ef158
Roi Gabay <rgabay@lightricks.com>
2020-04-26 18:01:05 +0300

    Created a history view (empty for now)

---
+++ Matcha/HistoryViewController.h
@@ -0,0 +1,17 @@
+//
+//  HistoryViewController.h
+//  Matcha
+//
+//  Created by Roi Gabay on 26/04/2020.
+//  Copyright © 2020 Roi Gabay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HistoryViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Matcha/HistoryViewController.m
@@ -0,0 +1,13 @@
+//
+//  HistoryViewController.m
+//  Matcha
+//
+//  Created by Roi Gabay on 26/04/2020.
+//  Copyright © 2020 Roi Gabay. All rights reserved.
+//
+
+#import "HistoryViewController.h"
+
+@implementation HistoryViewController
+
+@end
